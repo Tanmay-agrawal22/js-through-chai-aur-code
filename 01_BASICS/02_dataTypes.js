a591bb5e10747e5296683bd99fff295aa81e3728
Tanmay-agrawal22 <22tanmay13@gmail.com>
2025-10-19 11:49:56 +0000

    data type

--- 01_BASICS/02_dataTypes.js
+++ 01_BASICS/02_dataTypes.js
@@ -4,5 +4,23 @@ alert(3+3) //aler statement can't be used like this cuz using js not browser
 
 console.log(3
     +3
-) //it'll work but code readibility should be high which isn't here 
+//it'll work but code readibility should be high which isn't here 
+) 
+//Data types 
 
+let name = "tanmay" //string
+let age = 19 //numer
+let isloggedin = false //boolean
+
+//number => 2 to the power 53
+// bigint => for large numbers 
+//string => in double quotes ""
+//Boolean => yes or no 
+//null => stand alone value, represents empty value  
+//undefined =>when value not defined 
+//symbol => for uniqueness 
+
+//to kmow which type of data type(typeof)
+
+console.log(typeof null);//object
+console.log(typeof undefined);// undefined
