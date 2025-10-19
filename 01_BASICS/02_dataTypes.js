bceea193357d83dab75c59008774365903b8d8e0
Tanmay-agrawal22 <22tanmay13@gmail.com>
2025-10-19 10:13:28 +0000

    config done

---
+++ 01_BASICS/02_dataTypes.js
@@ -0,0 +1,8 @@
+"use strict"; //it makes all js codes in newest vesrsion
+
+alert(3+3) //aler statement can't be used like this cuz using js not browser 
+
+console.log(3
+    +3
+) //it'll work but code readibility should be high which isn't here 
+
--- 01_BASICS/02_variables.js
+++
@@ -1,3 +0,0 @@
-"use strict"; //it makes all js codes in newest vesrsion
-
-alert(3+3) //aler statement can't be used like this cuz using js not browser 
